2281925edc8cb48f75449f1343b0281dda053b81
Alexander <alexanderwulkan@hotmail.com>
2017-01-29 21:38:41 +0100

    Prep repo for Heroku

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,8 @@
 const axios = require("axios");
 
-const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?&appid=YOUR_APP_ID&units=metric";
+const OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?&appid=924312b06b6b0478955a0e4888e6828e&units=metric";
+
+// 924312b06b6b0478955a0e4888e6828e
 
 module.exports = {
     getTemp: function (location) {
